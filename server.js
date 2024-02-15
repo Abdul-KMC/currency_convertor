68a6dff8c979881ca5798374e08cd9d956e68f25
Abdul-KMC <mfnadwi0522@gmail.com>
2024-02-15 17:14:14 +0530

    feat: added currency-countryName route

--- server.js
+++ server.js
@@ -2,21 +2,22 @@ const express = require('express') // We import the express application
 require('dotenv').config();
 const currencyRouter = require('./routers/currency')
 const countryRouter = require('./routers/country')
+const currencyCountryRoute = require('./routers/currencyCountry')
 const middleware = require('./utils/middleware')
 const sequelize = require('./config/sequelize')
-const cors = require('cors') // Necessary for localhost
-const app = express() // Creates an express application in app
-
-/**
- * Initial application setup
- * We need to use cors so we can connect to a localhost later
- * We need express.json so we can receive requests with JSON data attached
- */
+const cors = require('cors')
+const app = express()
+    /**
+     * Initial application setup
+     * We need to use cors so we can connect to a localhost later
+     * We need express.json so we can receive requests with JSON data attached
+     */
 app.use(cors())
 app.use(express.json())
 app.use(middleware.morganMiddleware);
 app.use('/api/currency', currencyRouter);
 app.use('/api/country', countryRouter);
+app.use('/api/currency-countryName', currencyCountryRoute);
 
 // Unknown endpoint
 app.use(middleware.unknownHandler)
