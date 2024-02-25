c4123b7499c7681d7004a9ef08214e3970dfb3aa
Abdul-KMC <mfnadwi0522@gmail.com>
2024-02-25 15:35:23 +0530

    feat: added currency_utils to front-end

--- client/src/components/Convertor.jsx
+++ client/src/components/Convertor.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import '../App.css'
+import { convertCurrency } from '../utils/currency_utils';
+import '../App.css';
 
-function Convertor() {
+function Convertor(props) {
   const [formData, setFormData] = useState({
     currencyfrom: '',
     currencyto: '',
-    amount: ''
+    amount: 0.0
   });
 
   const handleInputChange = (e) => {
@@ -16,10 +17,21 @@ function Convertor() {
     }));
   };
 
-  const handleLogin = (e) => {
+  const handleConvert = (e) => {
     e.preventDefault();
 
     // Implement the conversion logic here
+    const { currencyfrom, currencyto, amount } = formData;
+    const fromCurrencyObj = props.currencies.find(curr => curr.currencyCode === currencyfrom);
+    const toCurrencyObj = props.currencies.find(curr => curr.currencyCode === currencyto);
+
+    if (fromCurrencyObj && toCurrencyObj) {
+      const result = convertCurrency(fromCurrencyObj, toCurrencyObj, amount);
+
+      alert(`${amount} ${currencyfrom} is ${result.toFixed(2)} ${currencyto}`);
+    } else {
+      alert('Invalid currency codes');
+    }
 
     // Clear the form data after attempting conversion
     setFormData({
@@ -32,31 +44,37 @@ function Convertor() {
   return (
     <div>
       <h1 className="title">Convertor</h1>
-      <form className='operationsForm' onSubmit={handleLogin}>
+      <form className='operationsForm' onSubmit={handleConvert}>
         <section className="inputField">
           <label>Currency From:</label>
-          <input
-            type="text"
+          <select
             name="currencyfrom"
-            placeholder="Currency From"
             value={formData.currencyfrom}
             onChange={handleInputChange}
             required
-          />
+          >
+            <option value="" disabled>Select Currency</option>
+            {props.currencies.map(curr => (
+              <option key={curr.id} value={curr.currencyCode}>{curr.currencyCode}</option>
+            ))}
+          </select>
         </section>
 
         <section className="inputField">
           <label>Currency To:</label>
-          <input
-            type="text"
+          <select
             name="currencyto"
-            placeholder="Currency To"
             value={formData.currencyto}
             onChange={handleInputChange}
             required
-          />
+          >
+            <option value="" disabled>Select Currency</option>
+            {props.currencies.map(curr => (
+              <option key={curr.id} value={curr.currencyCode}>{curr.currencyCode}</option>
+            ))}
+          </select>
         </section>
-        
+
         <section className="inputField">
           <label>Amount:</label>
           <input
@@ -68,13 +86,13 @@ function Convertor() {
             required
           />
         </section>
-        
+
         <div className="buttons">
           <button className="convertButton" type="submit">Convert</button>
         </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default Convertor
+export default Convertor;
